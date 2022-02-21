28e8638924688c0059cb159636ae34765919d3d5
carlos <chcruz64@gmail.com>
2022-02-21 19:23:53 -0300

    up

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector(".menu-header")
+let boxMenu = document.querySelector(".boxMenu")
+let item = document.querySelectorAll(".boxMenu > div")
+
+
+btn.addEventListener("click", () => {
+
+    boxMenu.classList.toggle("actived")
+    item.forEach( e => {
+
+        e.classList.toggle("item")
+        
+    })
+
+})
